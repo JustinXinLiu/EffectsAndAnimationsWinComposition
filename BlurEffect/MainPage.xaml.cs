9912542ed1aaaa5aceecee49adf7902cbc70b295
Justin Liu <justin.xin.liu@gmail.com>
2015-08-15 22:55:44 +1000

    Typo...

--- BlurEffect/MainPage.xaml.cs
+++ BlurEffect/MainPage.xaml.cs
@@ -10,7 +10,7 @@ using Windows.UI.Xaml.Input;
 namespace BlurEffect
 {
     /// <summary>
-    /// Please note that I could simply things by using the TouchArea Rectangle as the container visual to host the effect visual, 
+    /// Please note that I could simplify things by using the TouchArea Rectangle as the container visual to host the effect visual, 
     /// but I wanted to prove a point that I could use expression key frame animation to manipulate multiple elements at the same time!
     /// </summary>
     public sealed partial class MainPage : Page
