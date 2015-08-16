4944478d47da82e9ba91c794cb8bcafb5c6222b9
Justin Liu <justin.xin.liu@gmail.com>
2015-08-17 00:59:19 +1000

    Updated with a frosted glass effect based on Win2D gaussian blur.

--- BlurEffect/MainPage.xaml.cs
+++ BlurEffect/MainPage.xaml.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.Graphics.Canvas.Effects;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.Text;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Numerics;
+using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace BlurEffect
 {
@@ -21,10 +28,14 @@ namespace BlurEffect
         Vector3KeyFrameAnimation _animation;
         float _x;
 
+        CanvasBitmap _bitmap;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        #region Composition
 
         async void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
         {
@@ -39,9 +50,7 @@ namespace BlurEffect
             var image = _compositor.DefaultGraphicsDevice.CreateImageFromUri(new Uri("ms-appx:///Assets/White.png"));
             await image.CompleteLoadAsync();
 
-            // todo: not sure why GaussianBlurEffect doesn't work??
-            // Got a feeling it might have something to do with the Source setting, 
-            // maybe it's just not supported yet?
+            // currently GaussianBlurEffect is not supported in Composition
             var effectDefination = new SaturationEffect // new GaussianBlurEffect
             {
                 //BorderMode = EffectBorderMode.Soft,
@@ -100,9 +109,69 @@ namespace BlurEffect
             _touchAreaVisual.ConnectAnimation("Offset", _animation).Start();
         }
 
-        private static ContainerVisual GetVisual(FrameworkElement element)
+        static ContainerVisual GetVisual(FrameworkElement element)
         {
             return (ContainerVisual)ElementCompositionPreview.GetContainerVisual(element);
         }
+
+        #endregion
+
+        #region Win2D
+
+        void Canvas_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        async Task CreateResourcesAsync(CanvasControl sender)
+        {
+            // here I simply create the bitmap based on an existing image file,
+            // check the LoadAsync overload methods, you can also create one from
+            // an IRandomAccessStream
+            _bitmap = await CanvasBitmap.LoadAsync(sender, "Assets/Food.jpg");
+        }
+
+        void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            using (var session = args.DrawingSession)
+            {
+                var blur = new GaussianBlurEffect
+                {
+                    BlurAmount = 100.0f, // increase this to make it more blurry or vise versa.
+                    //Optimization = EffectOptimization.Balanced, // default value
+                    //BorderMode = EffectBorderMode.Soft // default value
+                    Source = _bitmap
+                };
+
+                session.DrawImage(blur, new Rect(0, 0, sender.ActualWidth, sender.ActualHeight),
+                    new Rect(0, 0, _bitmap.SizeInPixels.Width, _bitmap.SizeInPixels.Height), 0.9f);
+            }
+        }
+
+        void Overlay_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            // reset the inital with of the Rect
+            _x = (float)this.ImagePanel2.ActualWidth;
+        }
+
+        void Overlay_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            // get the movement on X axis
+            _x += (float)e.Delta.Translation.X;
+
+            // keep the pan within the bountry
+            if (_x > this.ImagePanel2.ActualWidth || _x < 0) return;
+
+            // we clip the overlay to reveal the actual image underneath
+            this.Clip.Rect = new Rect(0, 0, _x, this.ImagePanel2.ActualHeight);
+        }
+
+        void Overlay_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            // reset the clip to show the full overlay
+            this.Clip.Rect = new Rect(0, 0, this.ImagePanel2.ActualWidth, this.ImagePanel2.ActualHeight);
+        }
+
+        #endregion
     }
 }
